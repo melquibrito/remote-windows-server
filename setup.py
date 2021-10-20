e03553c692041b897112b8b21aa7a06f1680819b
Melqui Brito <45269246+melquibrito@users.noreply.github.com>
2021-10-20 16:52:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'rwin',
   packages = ['rwin'],
-  version = '0.0.1', 
+  version = '0.0.2', 
   license='MIT',
   description = 'This package provides a class with handy methods for running windows commands, executing powershell scripts, managing services, managing processes and getting information on windows remote servers.',
   author = 'Melquisedeque Brito de Lima',
@@ -10,12 +10,15 @@ setup(
   url = 'https://github.com/melquibrito/remote-windows-server',
   download_url = 'https://github.com/melquibrito/remote-windows-server/archive/refs/tags/v0.0.1.tar.gz',
   keywords = ['remote', 'windows', 'powershell'], 
-  install_requires=['winrm', 'logging'],
+  install_requires=['pywinrm', 'logging'],
   classifiers=[
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: >=3.6',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
   ],
 )
